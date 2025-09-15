2688f0cc09740899a04d182e97d5899baf91a155
tinyzqh <tinyzqh@gmail.com>
2025-09-15 21:14:04 +0900

    fix bug

--- OptiVerse/envs/video/VideoStreaming.py
+++ OptiVerse/envs/video/VideoStreaming.py
@@ -132,6 +132,9 @@ class VideoStreamingEnv(gym.Env):
             throughput = self.current_bandwidth[self.bandwidth_ptr] * B_IN_MB / BITS_IN_BYTE
             duration = self.current_trace_times[self.bandwidth_ptr] - self.last_bandwidth_time
             assert duration >= 0, "duration time must > 0! {} - {}".format(self.current_trace_times[self.bandwidth_ptr], self.last_bandwidth_time)
+            if duration <= 0:
+                print("self.bandwidth_ptr", self.bandwidth_ptr)
+                print("duration time must > 0! {} - {}".format(self.current_trace_times[self.bandwidth_ptr], self.last_bandwidth_time))
             packet_payload = throughput * duration * PACKET_PAYLOAD_PORTION
 
             if video_chunk_have_processed + packet_payload > selected_chunk_size:
